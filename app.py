9ca833ed65a031c6b41a55136ed41feb5dfc11cd
DE-Lupo <juergen.lupberger@mail.de>
2026-05-03 09:36:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -109,16 +109,17 @@ elif mode == "KI: Foto → gleiche Person als Zeichnung":
             with st.spinner("KI erstellt Zeichnung..."):
 
                 output = replicate.run(
-                    "stability-ai/sdxl:2f779eb9b23b34fe171f8eaa021b8261566f0d2c10cd2674063e7dbcd351509e",
-                    input={
-                        "image": file_to_bytes(uploaded_file),
-                        "prompt": prompt,
-                        "prompt_strength": 0.2,
-                        "num_inference_steps": 40,
-                        "guidance_scale": 5,
-                        "num_outputs": 1
-                    }
-                )
+    "stability-ai/sdxl:2f779eb9b23b34fe171f8eaa021b8261566f0d2c10cd2674063e7dbcd351509e",
+    input={
+        "image": file_to_bytes(uploaded_file),
+        "prompt": prompt,
+        "negative_prompt": "photo, color, realistic photo, painting, 3d render, smooth skin",
+        "prompt_strength": 0.45,
+        "num_inference_steps": 45,
+        "guidance_scale": 8,
+        "num_outputs": 1
+    }
+)
 
             result_url = get_result_url(output)
 
